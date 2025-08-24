b7c3d9eb47b1de0f9668b9c6587976242340f74a
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-08-24 20:17:57 +0700

    update: remove unused variable

--- app/src/main/java/com/example/videoapp/components/AuthInputComponent.kt
+++ app/src/main/java/com/example/videoapp/components/AuthInputComponent.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue

53d496d8d9408630293c9f7edb324545806c6fec
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-09-01 10:19:11 +0700

    create: video list screen as home screen

---
+++ app/src/main/java/com/example/videoapp/pages/video/VideoListScreen.kt
@@ -0,0 +1,27 @@
+package com.example.videoapp.pages.video
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun VideoListScreen(navHostController: NavHostController) {
+    Scaffold { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Video List Page")
+        }
+    }
+}

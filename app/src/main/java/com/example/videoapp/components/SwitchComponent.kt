7f8aa4b329fa77174be542dfdb561346e8576e8d
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-09-08 20:23:50 +0700

    create: switch component

---
+++ app/src/main/java/com/example/videoapp/components/SwitchComponent.kt
@@ -0,0 +1,35 @@
+package com.example.videoapp.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SwitchComponent(
+    value: Boolean,
+    onValueChange: (Boolean) -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            "Aktifkan Kolom Komentar",
+            style = TextStyle(
+                fontSize = 17.sp
+            )
+        )
+        Switch(
+            checked = value,
+            onCheckedChange = { onValueChange(it) }
+        )
+    }
+}

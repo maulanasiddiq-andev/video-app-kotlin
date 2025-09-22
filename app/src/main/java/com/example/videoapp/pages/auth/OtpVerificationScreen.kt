2a3ca7ae05630d087d6df55b9bb27637946ef2b6
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-09-22 14:35:05 +0700

    update: timer and button submit in otp verification page

--- app/src/main/java/com/example/videoapp/pages/auth/OtpVerificationScreen.kt
+++ app/src/main/java/com/example/videoapp/pages/auth/OtpVerificationScreen.kt
@@ -4,6 +4,7 @@ import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,9 +14,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -45,12 +48,20 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.videoapp.repositories.AuthRepository
 import com.example.videoapp.viewModels.auth.rememberRegisterViewModel
+import kotlinx.coroutines.delay
 import kotlin.collections.listOf
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 @Composable
 fun OtpVerificationScreen(navHostController: NavHostController, repository: AuthRepository) {
     val context = LocalContext.current
     val focusManager = LocalFocusManager.current
+
+    val countDownSeconds: Duration = (60 * 15).seconds
+    var isTimerRunning by remember { mutableStateOf(true) }
+    var timeLeft by remember { mutableStateOf(countDownSeconds) }
+
     var values by remember { mutableStateOf(listOf("", "", "", "")) }
     val focusRequesters = remember {
         listOf(FocusRequester(), FocusRequester(), FocusRequester(), FocusRequester())
@@ -58,7 +69,7 @@ fun OtpVerificationScreen(navHostController: NavHostController, repository: Auth
     var focusedIndex by remember { mutableIntStateOf(-1) }
 
     val viewModel = rememberRegisterViewModel(navHostController, repository = repository)
-//    val isLoading = viewModel.isLoadingOtp.value
+    val isLoading = viewModel.isLoadingOtp.value
     val succeed = viewModel.otpSucceed.value
     val message = viewModel.message.value
 
@@ -75,13 +86,29 @@ fun OtpVerificationScreen(navHostController: NavHostController, repository: Auth
         }
     }
 
+    LaunchedEffect(Unit, isTimerRunning) {
+        while (timeLeft > 0.seconds && isTimerRunning) {
+            delay(1.seconds)
+            timeLeft -= 1.seconds
+        }
+    }
+
     fun submit() {
+        isTimerRunning = false
+
         var value = ""
         values.forEach {
             value += it
         }
 
         viewModel.checkOtpValidation(value)
+    }
+
+    fun resendOTP() {
+        isTimerRunning = false
+
+        timeLeft = countDownSeconds
+        isTimerRunning = true
     }
 
     Scaffold { padding ->
@@ -124,6 +151,15 @@ fun OtpVerificationScreen(navHostController: NavHostController, repository: Auth
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Spacer(modifier = Modifier.height(60.dp))
+                Text(
+                    "Kode OTP sudah dikirim ke email@example.com",
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        lineHeight = 28.sp,
+                        textAlign = TextAlign.Center
+                    )
+                )
+                Spacer(modifier = Modifier.height(30.dp))
                 Row(
                     horizontalArrangement = Arrangement.spacedBy(15.dp)
                 ) {
@@ -182,6 +218,59 @@ fun OtpVerificationScreen(navHostController: NavHostController, repository: Auth
                                 )
                             )
                         }
+                    }
+                }
+                Spacer(modifier = Modifier.height(40.dp))
+                val formatted = "%02d:%02d".format(timeLeft.inWholeMinutes, timeLeft.inWholeSeconds % 60)
+                Text(
+                    formatted,
+                    style = TextStyle(
+                        fontSize = 20.sp
+                    )
+                )
+                Spacer(modifier = Modifier.height(40.dp))
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(5.dp)
+                ) {
+                    Text(
+                        "Belum menerima kode OTP?",
+                        style = TextStyle(
+                            fontSize = 16.sp
+                        )
+                    )
+                    Text(
+                        "Kirim ulang",
+                        modifier = Modifier.clickable{resendOTP()},
+                        style = TextStyle(
+                            color = Color(0xFF2196f3),
+                            fontSize = 16.sp
+                        )
+                    )
+                }
+                Spacer(modifier = Modifier.height(60.dp))
+                Box(
+                    modifier = Modifier
+                        .clickable{submit()}
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(Color(0xFF2196f3))
+                        .padding(15.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    if (isLoading) {
+                        CircularProgressIndicator(
+                            color = Color(0xffffffff),
+                            modifier = Modifier.size(17.dp),
+                            strokeWidth = 3.dp
+                        )
+                    } else {
+                        Text(
+                            "Kirim",
+                            style = TextStyle(
+                                fontSize = 16.sp,
+                                color = Color.White
+                            )
+                        )
                     }
                 }
             }
